f33748a5e16783e7844926a6674a6ef85ba274b8
DESKTOP-DD\Dimitar <dimitar@megantic.net.au>
2019-01-14 15:48:54 +0100

    Created Navbar, Search and ImageResults components. Populated Navbar and Search. Also quick cleanup on unnecessary things.

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+function Navbar() {
+  return (
+    <div>
+      <AppBar title='React-Image-Finder' />
+    </div>
+  )
+}
+
+export default Navbar;
